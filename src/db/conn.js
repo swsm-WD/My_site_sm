01656ad3f91416e2f12ef72a22f94c3e5f2e8aab
unknown <technologyyup@gmail.com>
2022-10-18 20:06:46 +0530

    commit message

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 //creating db
-mongoose.connect("mongodb://localhost:27017/My_site", {
+mongoose.connect("mongodb+srv://batch42:batch42@batch42.cm4zxfe.mongodb.net/zomDB?retryWrites=true&w=majority", {
    }).then(() => {
     console.log("connection successfull");
 }).catch((error) => {
